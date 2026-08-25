553576cf98293a5d007a7705b893522662a40217
veena <veenabadnikai8147@gmail.com>
2026-08-25 11:50:53 +0530

    save this

--- name.py
+++ name.py
@@ -1,4 +1,6 @@
 name = input("enter your name:")
 surname = input("enter your surname:")
-
+contact = input("enter your contact number:")
+print("your contact is:",contact)
 print("your fullname is:",name+""+surname)
+
